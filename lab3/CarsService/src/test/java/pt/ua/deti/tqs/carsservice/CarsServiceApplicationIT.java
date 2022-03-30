0e0386dd9a47871020e580d5ccc9959b3af115bd
Hugo Gonçalves <hugomiguel_2001_@sapo.pt>
2022-03-30 19:33:18 +0100

    Changed CarsServiceApplicationIT test name to add IT suffix.

--- lab3/CarsService/src/test/java/pt/ua/deti/tqs/carsservice/CarsServiceApplicationIT.java
+++ lab3/CarsService/src/test/java/pt/ua/deti/tqs/carsservice/CarsServiceApplicationIT.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -18,7 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(locations = "application-integration.properties")
-class CarsServiceApplicationRealDbTest {
+class CarsServiceApplicationIT {
 
     @LocalServerPort
     int serverPort;
